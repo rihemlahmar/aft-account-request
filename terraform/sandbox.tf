0bcaa601823e22388168819690c876221fd670ab
rihemlahmar <rihemlahmar01@gmail.com>
2024-04-04 14:28:09 +0200

    add sandbox account

--- terraform/sandbox.tf
+++ terraform/sandbox.tf
@@ -1,20 +1,20 @@
-module "sandbox_account_01" {
+module "sandbox_account" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "sandbox-account01@gmail.com"
+    AccountEmail = "sandbox-account-1@gmail.com"
     AccountName  = "sandbox"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "ou-6vtm-lh4mw0p2"
+    ManagedOrganizationalUnit = "sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "sandbox-account01@gmail.com"
+    SSOUserEmail     = "sandbox-account1@gmail.com"
     SSOUserFirstName = "sandbox"
     SSOUserLastName  = "account"
   }
 
   account_tags = {
-    "ABC:Owner"       = "sandbox-account01@gmail.com"
+    "ABC:Owner"       = "sandbox-account1@gmail.com"
     "ABC:Environment" = "Test"
   }
 
