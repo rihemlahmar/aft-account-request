7aaf9a6e16a0c3ca5bcfae11612fc26bad699de9
rihemlahmar <rihemlahmar01@gmail.com>
2024-04-04 14:39:43 +0200

    add sandbox account

--- terraform/account-request-sandbox.tf
+++ terraform/account-request-sandbox.tf
@@ -8,7 +8,7 @@ module "sandbox_account_001" {
     AccountEmail = "sandboxaccount001@gmail.com"
     AccountName  = "sandbox-account-001"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "sandbox"
+    ManagedOrganizationalUnit = "ou-6vtm-lh4mw0p2"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
     SSOUserEmail     = "sandboxaccount001@gmail.com"
