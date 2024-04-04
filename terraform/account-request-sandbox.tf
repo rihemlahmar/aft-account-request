e848157a52f659dca6fd4763242ca2a351fc71f8
rihemlahmar <rihemlahmar01@gmail.com>
2024-04-04 14:33:00 +0200

    add sandbox account

---
+++ terraform/account-request-sandbox.tf
@@ -0,0 +1,41 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+module "sandbox_account_001" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "sandboxaccount001@gmail.com"
+    AccountName  = "sandbox-account-001"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "sandboxaccount001@gmail.com"
+    SSOUserFirstName = "sandbox"
+    SSOUserLastName  = "account"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "sandboxaccount001@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Test"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem lahmar"
+    change_reason       = "testing the sandbox account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
