bc58bee263f9fea13c351de0d2145df208b125ab
rihemlahmar <rihemlahmar01@gmail.com>
2024-04-04 15:24:44 +0200

    add infra prod account

--- terraform/account-request-infra.tf
+++ terraform/account-request-infra.tf
@@ -39,3 +39,42 @@ module "network_test_account_001" {
 
   account_customizations_name = "sandbox-customizations"
 }
+
+module "network_prod_account_001" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "network-prod-account-001@gmail.com"
+    AccountName  = "network-prod-account-001"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "prod(ou-6vtm-eqj0akua)"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "network-prod-account-001@gmail.com"
+    SSOUserFirstName = "network"
+    SSOUserLastName  = "prod"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "network-prod-account-001@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Prod"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem lahmar"
+    change_reason       = "testing the infra network prod account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
